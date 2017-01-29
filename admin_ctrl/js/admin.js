74aa8a95b2010e89baf0543d1f834f1ffba44882
Jyotirmoy Nath <er.jyotirmoy.nath@gmail.com>
2017-01-29 22:47:48 +0530

    Add files via upload

--- admin_ctrl/js/admin.js
+++ admin_ctrl/js/admin.js
@@ -1,25 +1,23 @@
 (function(){
-	/*var app = angular.module('admin', []);
-	app.controller('Productedit', ['$http', function($http){
+	var app1 = angular.module('admin', ["ngRoute","ngTable","ngCookies"]);
+	app1.service('getallproducts', ['$http', function($http){
+		this.allproducts = function(){
+			return $http.get("products_get.php");
+		}
+	}])
+	app1.controller('Productedit', ['$http','NgTableParams','$location','$cookies','getallproducts', function($http,NgTableParams,$location,$cookies,getallproducts){
 		var edit = this;
 		edit.products = [];
-		$http.get('products.json').then(function(data){
+		edit.uname = $cookies.get('id');
+		getallproducts.allproducts().then(function(data){
 			edit.products = data.data;
-		}).catch(function(err){
-			console.log(err);
-		});
-	}]);*/
-	var app1 = angular.module('admin', ["ngRoute"]);
-	app1.controller('Productedit', ['$http', function($http){
-		var edit = this;
-		edit.products = [];
-		$http.get('products_get.php').then(function(data){
-			
-			edit.products = data.data;
+			edit.tableParams = new NgTableParams({}, { dataset: edit.products});
 			return edit.products;
-		}).catch(function(err){
-			console.log(err);
 		});
+		edit.logout = function(){
+				$cookies.remove('id');
+				$location.path("/login");
+		};
 	}]);
 	app1.controller('Addproducts',function($http){
 
@@ -40,29 +38,72 @@
 				});
 		};
 	});
-	app1.controller('EditProduct', function($scope,$routeParams,$http){
+	app1.service('getproducts', ['$http', function($http){
+
+		this.getprod = function(dataid){
+			return $http.post('product.php', dataid);
+		}
+	}])
+	app1.controller('EditProduct', function($scope,$routeParams,$http,getproducts){
 		
 			$scope.product = [];
-			$scope.data = [];
-
 			$scope.ID = $routeParams.ID;
-			$http.post('product.php',$scope.ID).then(function(data){
-				console.log(data.data[0]);
-				
-				$scope.product = angular.copy(angular.fromjson(data.data));
-				
-			}).catch(function(err){
-					console.log(err);
+			getproducts.getprod($scope.ID).then(function(data){
+				$scope.product = data.data[0];
+				console.log($scope.product);
 			});
+	});
+	/*app1.factory('loginfactory', function($http){
+		
+		var factory = {};
+			factory.setlogin = function(){
+			$http.post('login_user.php').then(function(data){
+				return data.data;
+			});
+		}
+		return factory;
+		
+	});*/
+	app1.service('loginservice',  function($http){
+		this.setlogin = function(udata){
+			return $http.post('login_user.php',udata);
+		}
+		
+	})
+	app1.controller('loginctrl',function($scope,loginservice,$location,$cookies){
+		$scope.udata = {
+			"username":"root"
+		};
+		$scope.submit = function(){
+				
+				loginservice.setlogin($scope.udata).then(function(d){
+						$cookies.put('id', d.data["token"]);
+						console.log($cookies.get('id'));
 
+						$location.path("/editproduct");
+				});
+				
+		}
 	});
 	app1.config(function($routeProvider) {
     $routeProvider
     .when("/editproduct", {
-        templateUrl : "edit_products.php"
+       
+        resolve:{
+            "check": function($location,$cookies){
+            	 var usertoken = $cookies.get('id');
+                if(!usertoken){
+                    $location.path('/login');
+                }                
+            }
+        },
+        templateUrl: "edit_products.php"
     })
     .when("/addprod", {
         templateUrl : "add_products.php"
+    })
+     .when("/login", {
+        templateUrl : "login.php"
     })
     .when("/prod/:ID",{
     	templateUrl:"edit.php",	
